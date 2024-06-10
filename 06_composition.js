7a328e187ca32681f8788b66064f3dd27f8770f8
SMARCOCCIA <seb@gmail.fr>
2024-06-10 12:21:13 +0200

    Suite exo 6

--- 06_composition.js
+++ 06_composition.js
@@ -19,16 +19,16 @@ console.log(concat([1, 2, 3], [4, 5, 6]));
  * - ne pas utiliser la méthode push
  */
 
-const push = (arr, item) => {}
-console.log(push);
+const push = (arr, item) => [...arr, item]
+console.log(push([1, 2, 3], 4));
 /**
  * utiliser l'opérateur de composition ... afin de fusionner 2 objets passés en paramètres
  * 
  * ex: {a: 1, b: 2}, {c: 3, d: 4} => {a: 1, b: 2, c: 3, d: 4}
  */
 
-const merge = (obj1, obj2) => {}
-console.log(merge);
+const merge = (obj1, obj2) => [...[obj1], ...[obj2]]
+console.log(merge({a: 1, b: 2}, {c: 3, d: 4}));
 /**
  * utiliser l'opérateur de composition ... afin de modifier la propriété name de l'objet
  * 
@@ -38,7 +38,9 @@ console.log(merge);
  *  - interdiction d'utiliser l'opérateur d'affectation "="
  */
 
-const setName = (obj, name) => {}
-console.log(setName);
+const setName = (obj, name) => {
+    //return [...[obj]: name]
+}
+console.log(setName({name: 'toto'}, 'titi'));
 // astuce: {...obj} crée une copie de l'objet, c'est un des principes de l'immutabilité et évite les problèmes de référence
 module.exports = {concat, push, merge, setName}
